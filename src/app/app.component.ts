2c4b364f602df5713454937c1155f84342b6eb20
TamikoSRN <thomasereno1@gmail.com>
2022-06-09 23:00:17 -0300

    App worksgit add .!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './models/weather.model';
 import { WeatherService } from './services/weather.service';
 
 @Component({
@@ -11,11 +12,14 @@ export class AppComponent implements OnInit {
   constructor(private weatherService: WeatherService) {
 
   }
+
+  weatherData?: WeatherData;
   
   ngOnInit(): void {
     this.weatherService.getWeatherData('Buenos Aires')
     .subscribe({
       next: (response) => {
+        this.weatherData = response;
         console.log(response)
       }
     })
